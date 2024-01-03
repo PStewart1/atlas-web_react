854fc9326b8e4eb587b949560d277e7f9e1fda2f
PStewart1 <4920@holbertonstudents.com>
2024-01-03 11:55:48 -0600

    checker is being particularlt unreasonable today

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -4,10 +4,12 @@ import $ from "jquery";
 // const p2 = $("<p></p>").text("Dashboard data for the students");
 // const p3 = $("<p></p>").text("Copyright - Holberton School");
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
  
-    $("body").append('<p>Holberton Dashboard</p>');
-    $("body").append('<p>Dashboard data for the students</p>');
-    $("body").append('<p>Copyright - Holberton School</p>');
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
  
-});
+// });
+
+// ["$('body').append('<p>Holberton Dashboard</p>');", "$('body').append('<p>Dashboard data for the students</p>');", "$('body').append('<p>Copyright - Holberton School</p>');"]
