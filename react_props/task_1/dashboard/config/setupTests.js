da242344953ac4c3ff885034e863854a9445bf47
PStewart1 <4920@holbertonstudents.com>
2024-01-09 22:12:51 -0600

    Write the tests for each component

---
+++ react_props/task_1/dashboard/config/setupTests.js
@@ -0,0 +1,9 @@
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+
+configure({ adapter: new Adapter() });
+
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// Enzyme.configure({ adapter: new Adapter() });
