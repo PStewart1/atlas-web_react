fd323c6a7d92e46f8ae5dc8c4e28af7589528e35
PStewart1 <4920@holbertonstudents.com>
2024-01-09 19:54:14 -0600

    adjusted directory paths

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -3,30 +3,31 @@ const path = require('path');
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: '../src/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    path: path.resolve(__dirname, '../dist'),
+    // clean: true,
   },
   mode: 'development',
 
   devtool: 'inline-source-map',
   devServer: {
     static: {
-      directory: path.join(__dirname, './dist'),
+      directory: path.join(__dirname, '../dist'),
     },
+    hot: true,
     compress: true,
     port: 3000,
   },
 
-  plugins: [
+  // plugins: [
     // new HtmlWebpackPlugin({
     //     hash: true,
     //     filename: './index.html' //relative to root of the application
     // }),
     // new CleanWebpackPlugin(),
-  ],
+  // ],
 
   // optimization: {
   //   splitChunks: {
