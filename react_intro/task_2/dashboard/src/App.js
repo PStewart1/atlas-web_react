f48bef2fe3bbbff3a23ac896b6c74a0a9e1be0a5
PStewart1 <4920@holbertonstudents.com>
2024-01-09 18:20:55 -0600

    Modify the App

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,39 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email"></input>
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password"></input>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
