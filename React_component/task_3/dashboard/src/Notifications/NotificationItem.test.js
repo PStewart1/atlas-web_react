4be271395214254960beafca1d05cf3aa29f2814
PStewart1 <4920@holbertonstudents.com>
2024-01-11 21:35:59 -0600

    2. Handling Events

---
+++ React_component/task_3/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem.js';
+// import '../../config/setupTests.js';
+
+describe('<NotificationItem />', () => {
+  it('renders an <NotificationItem /> component', () => {
+    const wrapper = shallow(<NotificationItem />);
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders with props', () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.props().dataPriority === 'default');
+    expect(wrapper.props().dataValue === 'test');
+  });
+
+  it('renders an <NotificationItem /> component checking for html', () => {
+    const wrapper = shallow(<NotificationItem html={{ __html: '<u>test</u>' }} />);
+    expect(wrapper.html()).toEqual('<li data-priority="default"><u>test</u></li>');
+  });
+
+  // it('Passes a spy as the markAsRead property', () => {
+  //   const wrapper = shallow(<NotificationItem type="default" value="test" />);
+  //   const instance = wrapper.instance();
+  //   const spy = jest.spyOn(instance, 'markAsRead', 'get').mockReturnValue(() => 'test');
+  //   expect(instance.markAsRead()).toEqual('test');
+  // });
+
+  it('Check that when simulating a click on the component, the spy is called with the right ID argument', () => {
+    const wrapper = shallow(<NotificationItem id={1} />);
+    const instance = wrapper.instance();
+    const spy = jest.spyOn(global.console, 'log');
+    wrapper.find('li').simulate('click');
+    expect(spy).toHaveBeenCalledWith('Notification 1 has been marked as read');
+    spy.mockRestore();
+  });
+
+});
