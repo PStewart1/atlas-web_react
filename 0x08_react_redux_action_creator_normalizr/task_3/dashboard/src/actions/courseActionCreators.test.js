def61ec4d8feb4447e9369a982dca0fbc88a8e1b
PStewart1 <4920@holbertonstudents.com>
2024-02-05 22:52:47 -0600

    3. Create actions for the course list

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -0,0 +1,13 @@
+import { selectCourse, unSelectCourse } from "./courseActionCreators";
+
+describe("courseActionCreators", () => {
+  it("returns the correct action for selectCourse", () => {
+    const action = selectCourse(1);
+    expect(action).toEqual({ type: "SELECT_COURSE", index: 1 });
+  });
+
+  it("returns the correct action for unSelectCourse", () => {
+    const action = unSelectCourse(1);
+    expect(action).toEqual({ type: "UNSELECT_COURSE", index: 1 });
+  });
+});
