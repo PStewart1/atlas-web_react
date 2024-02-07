4c460f7958d50b29e312fa05383a3dfc13e39cad
PStewart1 <4920@holbertonstudents.com>
2024-02-06 19:43:21 -0600

    3. Create the reducer for notifications

---
+++ react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,47 @@
+import {MARK_AS_READ, SET_TYPE_FILTER, FETCH_NOTIFICATIONS_SUCCESS } from './notificationActionTypes';
+import { bindActionCreators } from "redux";
+
+export const markAsAread  = (index) => {
+  return {
+    type: MARK_AS_READ,
+    index
+  }
+};
+
+export const setNotificationFilter = (filter) => {
+  return {
+    type: SET_TYPE_FILTER,
+    filter
+  }
+};
+
+export const fetchNotificationsSuccess = (data) => {
+  return {
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    data: [
+      {
+        id: 1,
+        type: "default",
+        value: "New course available"
+      },
+      {
+        id: 2,
+        type: "urgent",
+        value: "New resume available"
+      },
+      {
+        id: 3,
+        type: "urgent",
+        value: "New data available"
+      }
+    ]
+  }
+}
+
+export const notificationActions = {
+  markAsAread,
+  setNotificationFilter,
+  fetchNotificationsSuccess
+};
+
+export const boundNotificationActions = (dispatch) => bindActionCreators(notificationActions, dispatch);
